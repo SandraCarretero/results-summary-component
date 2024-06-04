948f4df8831941cb9cb714fe5aeca727c80b26da
SandraCarretero <sandracarretero24@gmail.com>
2024-06-04 09:42:48 +0200

    css-modules

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,11 +1,13 @@
-import Result from '../result/Result'
-import Summary from '../summary/Summary'
-import style from './container.module.css'
-const Container =()=>{
-    return <div className={style.container}>
-        <Result></Result>
-        <Summary></Summary>
-    </div>
-}
+import Result from '../result/Result';
+import Summary from '../summary/Summary';
+import style from './container.module.css';
+const Container = () => {
+	return (
+		<div className={style.container}>
+			<Result></Result>
+			<Summary></Summary>
+		</div>
+	);
+};
 
-export default Container
+export default Container;
